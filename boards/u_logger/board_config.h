9478c93e2c9cc15e524d5f75bc2122644455c97e
Dirk Hammersley <danaan.metge@gmail.com>
2021-06-04 09:53:51 -0400

    initial addition of utility datalogger config

---
+++ boards/u_logger/board_config.h
@@ -0,0 +1,20 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define VENDOR_NAME "Danaan"
+#define PRODUCT_NAME "Utility Datalogger"
+#define VOLUME_LABEL "DL_BOOT"
+
+#define BOARD_ID "SAMD21E18A-Datalogger-revA"
+
+//#define USB_VID 0x2341
+//#define USB_PID 0x024D
+
+#define LED_PIN PIN_PA10
+//#define LED_TX_PIN PIN_PA27
+//#define LED_RX_PIN PIN_PB03
+
+
+#define BOARD_NEOPIXEL_PIN PIN_PA00
+#define BOARD_NEOPIXEL_COUNT 1
+#endif
